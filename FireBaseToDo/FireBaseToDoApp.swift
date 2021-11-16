085a8b6fc710620841f56c1d80688c3c6f3870cf
Promal <promal.barua.cse@gmail.com>
2021-11-17 01:30:48 +0600

    Initial Commit

---
+++ FireBaseToDo/FireBaseToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseToDoApp.swift
+//  FireBaseToDo
+//
+//  Created by Promal on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
